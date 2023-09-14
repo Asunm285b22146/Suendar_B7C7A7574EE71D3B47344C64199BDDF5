21c3f7901e3bad17744c0ecf1e6cf06055cc5dbf
Asunm285b22146 <144418639+Asunm285b22146@users.noreply.github.com>
2023-09-14 11:12:56 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,11 +1,28 @@
-def isleapyear(year):
-  if(year%4==0 and year%100!=0 or year%400==0):
-    return True
-  else:
-    return False
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-year=int(input("Enter a year:"))
-if isleapyear(year):
-  print(year,"is a leap year")
-else:
-  print(year,"is not a leap year")
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return True
+        else:
+            return False
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        return f"Account Number: {self.__account_number}, Holder: {self.__account_holder_name}, Balance: {self.__account_balance}"
+account = BankAccount("123456789", "John Doe", 1000)
+print(account.display_balance())
+account.deposit(500)
+print(account.display_balance())
+account.withdraw(200)
+print(account.display_balance())
